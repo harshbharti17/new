5f8a50fd213cb540dda50f3ecd104d2d10c23797
HarshBharti17 <harshbharti179@gmail.com>
2025-12-06 20:10:48 +0530

    make it newsLetterBox component and some changes

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -16,8 +16,8 @@ const LatestCollection = () => {
       <div className="text-center pt-3 pb-8 text-3xl">
         <Title text1={"LATEST "} text2={"COLLECTION"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-          illum officia quibusdam voluptas.
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem ipsum has been the.
         </p>
       </div>
       {/* Rendering products */}
