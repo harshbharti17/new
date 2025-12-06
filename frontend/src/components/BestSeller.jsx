7c63b39f11d03a4b6cfb42cdadb6f99a393fa6fe
HarshBharti17 <harshbharti179@gmail.com>
2025-12-06 19:25:30 +0530

    adding bestseller component and make it responsive

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,31 +3,31 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProducts] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    setLatestProducts(products.slice(0, 10));
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
   }, []);
 
   return (
-    <div className="my-10 ">
-      <div className="text-center pt-3 pb-8 text-3xl">
-        <Title text1={"LATEST "} text2={"COLLECTION"} />
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST "} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-          illum officia quibusdam voluptas.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+          dolores libero debitis explicabo sunt.
         </p>
       </div>
-      {/* Rendering products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
             id={item._id}
-            image={item.image}
             name={item.name}
+            image={item.image}
             price={item.price}
           />
         ))}
@@ -36,4 +36,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
