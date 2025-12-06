3923dae26c99ec749fe8fa786de176537f81a625
HarshBharti17 <harshbharti179@gmail.com>
2025-12-06 15:03:44 +0530

    add Navbar link to homepage and update styles for active links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium ">
-      <img src={assets.logo} className="w-36" alt="" />
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700  ">
         <NavLink to="/" className="flex flex-col items-center gap-1 ">
@@ -83,21 +85,21 @@ const Navbar = () => {
           </NavLink>
           <NavLink
             onClick={() => setVisible(false)}
-            className="py-2 pl-6 border"
+            className="py-2 pl-6 border-b"
             to="/collection"
           >
             COLLECTION
           </NavLink>
           <NavLink
             onClick={() => setVisible(false)}
-            className="py-2 pl-6 border"
+            className="py-2 pl-6 border-b"
             to="/about"
           >
             ABOUT
           </NavLink>
           <NavLink
             onClick={() => setVisible(false)}
-            className="py-2 pl-6 border"
+            className="py-2 pl-6 border-b"
             to="/contact"
           >
             CONTACT
