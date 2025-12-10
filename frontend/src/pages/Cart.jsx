6a3ebce2c121bae61f7ed8f5a50eeca89ef09593
HarshBharti17 <harshbharti179@gmail.com>
2025-12-10 14:40:48 +0530

    adding placeOrder functionality and navigate to orders

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
@@ -91,8 +91,12 @@ const Cart = () => {
         <div className="w-full sm:w-[470px]">
           <CartTotal />
           <div className="w-full text-end ">
-            <button className="bg-black text-white text-sm my-8 px-8 py-3 rounded cursor-pointer">PROCEED TO CHECKOUT</button>
-
+            <button
+              onClick={() => navigate("/place-order")}
+              className="bg-black text-white text-sm my-8 px-8 py-3 rounded cursor-pointer"
+            >
+              PROCEED TO CHECKOUT
+            </button>
           </div>
         </div>
       </div>
