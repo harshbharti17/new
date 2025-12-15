76871edab9342ebf96fc435cb8aa11ac0472d4fd
HarshBharti17 <harshbharti179@gmail.com>
2025-12-15 18:32:40 +0530

    implement removeProduct function to delete a product by ID

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -84,7 +84,23 @@ const listProducts = async (req, res) => {
 };
 
 // removing product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+
+    res.json({
+      success: true,
+      message: "Product Removed",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // single product info
 const singleProduct = async (req, res) => {};
