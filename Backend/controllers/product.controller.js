b69918529360b48967494f593e899e1568ab0af3
HarshBharti17 <harshbharti179@gmail.com>
2025-12-15 18:25:10 +0530

    implement listProducts function to retrieve and return all products

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -65,7 +65,23 @@ const addProduct = async (req, res) => {
 };
 
 // list products
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+
+    res.json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 // removing product
 const removeProduct = async (req, res) => {};
