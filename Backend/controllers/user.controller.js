73114047d78ab97ccf7d76603a23c88181af68ab
HarshBharti17 <harshbharti179@gmail.com>
2025-12-15 19:13:16 +0530

    add adminLogin functionality

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -62,7 +62,7 @@ const registerUser = async (req, res) => {
       });
     }
 
-    if (password.length < 8) {
+    if (password.length <= 8) {
       return res.json({
         success: false,
         message: "Please enter a strong password",
@@ -97,6 +97,32 @@ const registerUser = async (req, res) => {
 };
 
 //Route for admin login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({
+        success: true,
+        token,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export { loginUser, registerUser, adminLogin };
