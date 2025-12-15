b87c11039f0d99286f420d921be14f7950814c89
HarshBharti17 <harshbharti179@gmail.com>
2025-12-15 13:22:04 +0530

    add a product controller with add, list, remove, single products

---
+++ Backend/controllers/product.controller.js
@@ -0,0 +1,31 @@
+
+
+// add product
+const addProduct = async (req, res) => {
+    
+}
+
+
+// list products
+const listProducts = async (req, res) => {
+    
+}
+
+
+// removing product
+const removeProduct = async (req, res) => {
+    
+}
+
+// single product info
+const singleProduct = async (req, res) => {
+    
+}
+
+
+export {
+   addProduct,
+   listProducts,
+   removeProduct,
+   singleProduct
+}
