4382ebe604fededa11f4c38191f7cb40c0336e00
HarshBharti17 <harshbharti179@gmail.com>
2026-01-12 20:58:13 +0530

    update List component to accept token prop

--- Admin/src/pages/List.jsx
+++ Admin/src/pages/List.jsx
@@ -2,11 +2,9 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { backendUrl, currency } from "../App";
 
-
 import { toast } from "react-toastify";
 
-
-const List = () => {
+const List = ({ token }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
@@ -25,11 +23,24 @@ const List = () => {
 
   const removeProduct = async (id) => {
     try {
-      
+      const response = await axios.post(
+        backendUrl + "/api/product/remove",
+        { id },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
     } catch (error) {
-      
+      console.log(error);
+
+      toast.error(error.message);
     }
-  }
+  };
 
   useEffect(() => {
     fetchList();
@@ -50,12 +61,23 @@ const List = () => {
 
         {/* -------product list-------- */}
         {list.map((item, index) => (
-          <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm " key={index}>
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm "
+            key={index}
+          >
             <img className="w-12" src={item.image[0]} alt="" />
             <p>{item.name}</p>
             <p>{item.category} </p>
-            <p>{currency}{item.price}</p>
-            <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p
+              onClick={() => removeProduct(item._id)}
+              className="text-right md:text-center cursor-pointer text-lg"
+            >
+              X
+            </p>
           </div>
         ))}
       </div>
