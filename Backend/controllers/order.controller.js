7afe7913820f0a31f5e3ddfc4955e6ff1042519d
HarshBharti17 <harshbharti179@gmail.com>
2026-01-14 16:21:40 +0530

    creating a order controllers for any orders

---
+++ Backend/controllers/order.controller.js
@@ -0,0 +1,40 @@
+
+
+//Placing orders using COD method
+const placeOrder = async (req, res) => {
+    
+}
+
+//Placing orders using Stripe method
+const placeOrderStripe = async (req, res) => {
+    
+}
+
+//Placing orders using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+    
+}
+
+//All orders data for admin panel
+const allOrders = async (req, res) => {
+    
+}
+
+//user order data for Frontend
+const userOrders = async (req, res) => {
+    
+}
+
+//update order status from admin panel
+const updateStatus = async (req, res) => {
+    
+}
+
+export{
+    placeOrder,
+    placeOrderStripe,
+    placeOrderRazorpay,
+    allOrders,
+    userOrders,
+    updateStatus
+}
