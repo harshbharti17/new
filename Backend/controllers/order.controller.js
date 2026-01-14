86b15de7e7aea2f017a09e4cc26ac5e4e9d07aa8
HarshBharti17 <harshbharti179@gmail.com>
2026-01-14 16:49:36 +0530

    adding a place order controller in orders file

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -1,40 +1,58 @@
-
+import orderModel from "../models/order.model.js";
+import userModel from "../models/user.model.js";
 
 //Placing orders using COD method
 const placeOrder = async (req, res) => {
-    
-}
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+
+    res.json({
+      success: true,
+      message: "Order Placed",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //Placing orders using Stripe method
-const placeOrderStripe = async (req, res) => {
-    
-}
+const placeOrderStripe = async (req, res) => {};
 
 //Placing orders using Razorpay method
-const placeOrderRazorpay = async (req, res) => {
-    
-}
+const placeOrderRazorpay = async (req, res) => {};
 
 //All orders data for admin panel
-const allOrders = async (req, res) => {
-    
-}
+const allOrders = async (req, res) => {};
 
 //user order data for Frontend
-const userOrders = async (req, res) => {
-    
-}
+const userOrders = async (req, res) => {};
 
 //update order status from admin panel
-const updateStatus = async (req, res) => {
-    
-}
+const updateStatus = async (req, res) => {};
 
-export{
-    placeOrder,
-    placeOrderStripe,
-    placeOrderRazorpay,
-    allOrders,
-    userOrders,
-    updateStatus
-}
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};
