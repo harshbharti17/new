2086d1e54dc3ded9f09584d4e517da7c8cf9ac51
HarshBharti17 <harshbharti179@gmail.com>
2026-01-15 19:40:14 +0530

    refactor Orders component for improved layout and styling consistency

--- Admin/src/pages/Orders.jsx
+++ Admin/src/pages/Orders.jsx
@@ -40,20 +40,23 @@ const Orders = ({ token }) => {
       <h3>Order Page</h3>
       <div>
         {orders.map((order, index) => (
-          <div key={index}>
-            <img src={assets.parcel_icon} alt="" />
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 "
+            key={index}
+          >
+            <img className="w-12" src={assets.parcel_icon} alt="" />
             <div>
               <div>
                 {order.items?.map((item, index) => {
                   if (index === order.items.length - 1) {
                     return (
-                      <p key={index}>
+                      <p className="py-0.5" key={index}>
                         {item.name} X {item.quantity} <span>{item.size} </span>{" "}
                       </p>
                     );
                   } else {
                     return (
-                      <p key={index}>
+                      <p className="py-0.5" key={index}>
                         {item.name} X {item.quantity} <span>{item.size} </span>{" "}
                         ,
                       </p>
@@ -61,7 +64,12 @@ const Orders = ({ token }) => {
                   }
                 })}
               </div>
-              <p>{order.address.firstName + " " + order.address.lastName} </p>
+              <p
+                className="mt-3 mb-2
+               font-medium"
+              >
+                {order.address.firstName + " " + order.address.lastName}{" "}
+              </p>
               <div>
                 <p>{order.address.street + ","}</p>
                 <p>
@@ -77,21 +85,23 @@ const Orders = ({ token }) => {
               <p>{order.address.phone}</p>
             </div>
             <div>
-              <p>Items : {order.items.length} </p>
-              <p>Method : {order.paymentMethod}</p>
+              <p className="text-sm sm:text-[15px]">
+                Items : {order.items.length}{" "}
+              </p>
+              <p className="mt-3">Method : {order.paymentMethod}</p>
 
               <p>Payment : {order.payment ? "Done" : "Panding"} </p>
               <p>Date : {new Date(order.date).toLocaleDateString()} </p>
             </div>
-            <p>
+            <p className="text-sm sm:text-[15px]">
               {currency}
               {order.amount}
             </p>
-            <select>
+            <select value={order.status} className="p-2 font-semibold">
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
-              <option value="Out for delivery">Out for delivery</option>
+              <option value="Out For Delivery">Out For Delivery</option>
               <option value="Delivered">Delivered</option>
             </select>
           </div>
