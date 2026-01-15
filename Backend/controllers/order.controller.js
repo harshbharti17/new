f0c614525b5187940d2a4b7ad287cfda5df5baa4
HarshBharti17 <harshbharti179@gmail.com>
2026-01-15 21:28:25 +0530

    add verifyStripe function to handle payment verification and order updates

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -101,6 +101,32 @@ const placeOrderStripe = async (req, res) => {
   }
 };
 
+//verify stripe
+const verifyStripe = async (req, res) => {
+  const { orderId, success, userId } = req.body;
+
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      await userModel.findByIdAndUpdate(userId, { cartData: {} });
+      res.json({
+        success: true,
+      });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 //Placing orders using Razorpay method
 const placeOrderRazorpay = async (req, res) => {};
 
