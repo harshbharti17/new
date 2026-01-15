3144a2b93e3a0e3d54ca0439579ce12800f4ecaf
HarshBharti17 <harshbharti179@gmail.com>
2026-01-15 19:51:52 +0530

    adding status handler function in orders component

--- Admin/src/pages/Orders.jsx
+++ Admin/src/pages/Orders.jsx
@@ -31,6 +31,23 @@ const Orders = ({ token }) => {
     }
   };
 
+  const statusHandler = async (e, orderId) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/order/status",
+        { orderId, status: e.target.value },
+        { headers: { token } }
+      );
+
+      if (response.data.success) {
+        await fetchAllOrders();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message);
+    }
+  };
+
   useEffect(() => {
     fetchAllOrders();
   }, [token]);
@@ -97,7 +114,11 @@ const Orders = ({ token }) => {
               {currency}
               {order.amount}
             </p>
-            <select value={order.status} className="p-2 font-semibold">
+            <select
+              onChange={(e) => statusHandler(e, order._id)}
+              value={order.status}
+              className="p-2 font-semibold"
+            >
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
