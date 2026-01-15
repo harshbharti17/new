0d10af164de113240fb4444e40d2036017b53a12
HarshBharti17 <harshbharti179@gmail.com>
2026-01-15 21:24:45 +0530

    adding place order stripe payment method functionality

--- Backend/controllers/order.controller.js
+++ Backend/controllers/order.controller.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/order.model.js";
 import userModel from "../models/user.model.js";
+import Stripe from "stripe";
+
+//global variables
+const currency = "inr";
+const deliveryCharge = 10;
+
+//gatway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //Placing orders using COD method
 const placeOrder = async (req, res) => {
@@ -35,7 +43,63 @@ const placeOrder = async (req, res) => {
 };
 
 //Placing orders using Stripe method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: deliveryCharge * 100,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id} `,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id} `,
+      line_items,
+      mode: "payment",
+    });
+
+    res.json({
+      success: true,
+      session_url: session.url,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //Placing orders using Razorpay method
 const placeOrderRazorpay = async (req, res) => {};
@@ -101,4 +165,5 @@ export {
   allOrders,
   userOrders,
   updateStatus,
+  verifyStripe,
 };
